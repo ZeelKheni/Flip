65729b7c668406007c4492dc54df27e27714cca5
Zeel-Kheni <zeel.kheni@noborderz.com>
2026-02-18 15:26:33 +0530

    Implemented Audio System

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -7,9 +7,11 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private AudioClip matchSound;
     [SerializeField] private AudioClip mismatchSound;
     [SerializeField] private AudioClip gameOverSound;
+    [SerializeField] private AudioClip uiClickSound; // New!
 
     public void PlayFlipSound() => sfxSource.PlayOneShot(flipSound);
     public void PlayMatchSound() => sfxSource.PlayOneShot(matchSound);
     public void PlayMismatchSound() => sfxSource.PlayOneShot(mismatchSound);
     public void PlayGameOverSound() => sfxSource.PlayOneShot(gameOverSound);
+    public void PlayUIClick() => sfxSource.PlayOneShot(uiClickSound); // New!
 }
